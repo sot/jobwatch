cba0acfab360875a6c55e66886cdc7f5592b436c
Javier Gonzalez <javierggt@yahoo.com>
2022-09-19 15:47:42 -0400

    re-enable astromon checks

--- jobwatch/skawatch.py
+++ jobwatch/skawatch.py
@@ -111,6 +111,7 @@ telem_archive_errs = copy_errs(py_errs, ['fail'],
                                ['(?<!...)fail(?!...)'])
 perigee_errs = copy_errs(py_errs, ['warn'],
                          ['warn(?!ing: limit exceeded, dac of)'])
+astromon_errs = ('error', 'fatal', 'fail')
 engarchive_errs = copy_errs(py_errs, ['fail'],
                             ['(?<!5OHW)FAIL(?!MODE)'])
 perigee_errs = copy_errs(py_errs, ['warn'],
@@ -136,6 +137,7 @@ def main():
         SkaJobWatch('aimpoint_mon', 2, errors=py_errs),
         SkaJobWatch('arc', 2, errors=perl_errs,
                     exclude_errors=arc_exclude_errors, logdir='Logs'),
+        SkaJobWatch('astromon', 8, errors=astromon_errs),
         SkaJobWatch('attitude_error_mon', 2, errors=att_mon_errs),
         SkaJobWatch('aca_weekly_report', 3, errors=py_errs,
                     filename='/proj/sot/ska/data/aca_weekly_report/logs/aca_weekly_report.log'),
