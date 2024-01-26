36bd2ceb1b5789b451f020385038c7ed8f2aa208
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-01-26 11:29:08 -0500

    More indented

--- jobwatch/hourly_watch.py
+++ jobwatch/hourly_watch.py
@@ -166,9 +166,9 @@ def main():
         jws = [
             SkaURLWatch('kadi', 1, 'https://kadi.cfa.harvard.edu'),
             FileWatch('kadi cmds', 1,
-                  filename='/proj/sot/ska/data/kadi/cmds.h5'),
+                      filename='/proj/sot/ska/data/kadi/cmds.h5'),
             FileWatch('kadi cmd events', 1,
-                    filename='/proj/sot/ska/data/kadi/cmd_events.csv')
+                      filename='/proj/sot/ska/data/kadi/cmd_events.csv')
         ]
     elif args.jobs == 'mta':
         jws = [
