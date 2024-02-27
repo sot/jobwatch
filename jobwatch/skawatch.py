e0bbbd9045372763e7f7fad3fe310e0a831e5b80
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-02-27 15:18:11 -0500

    Add a daily check that kadi cmds2 generally to future

--- jobwatch/skawatch.py
+++ jobwatch/skawatch.py
@@ -7,6 +7,7 @@ import time
 
 from cxotime import CxoTime
 from kadi import events
+import kadi.commands
 
 import jobwatch
 from jobwatch import (FileWatch, JobWatch, DbWatch,
@@ -75,6 +76,26 @@ class KadiWatch(JobWatch):
             self.filetime = CxoTime(last_dwell.stop).unix
             self.filedate = time.ctime(self.filetime)
             self._age = (CxoTime.now() - CxoTime(last_dwell.stop)).to_value(u.day)
+        return self._age
+
+
+class KadiCmdsWatch(JobWatch):
+    def __init__(self, task, filename, maxage=1):
+        self.type = 'Application Data'
+        super().__init__(task, filename, maxage=maxage)
+
+    @property
+    def filelines(self):
+        return []
+
+    @property
+    def age(self):
+        if not hasattr(self, '_age'):
+            cmds = kadi.commands.get_cmds(start=CxoTime.now() - 7 * u.day, scenario="flight")
+            last_cmd = cmds[-1]
+            self.filetime = CxoTime(last_cmd['date']).unix
+            self.filedate = time.ctime(self.filetime)
+            self._age = (CxoTime.now() - CxoTime(last_cmd['date'])).to_value(u.day)
         return self._age
 
 
@@ -221,6 +242,9 @@ def main():
     ])
 
     jws.extend([KadiWatch('kadi dwells', '/proj/sot/ska/data/kadi/events3.db3', maxage=3)])
+
+    # Commands should go out into the future unless we're in an anomaly state
+    jws.extend([KadiCmdsWatch('kadi cmds', '/proj/sot/ska/data/kadi/cmds2.h5', maxage=-1)])
 
     set_report_attrs(jws)
     index_html = make_html_report(jws, args.rootdir, args.date_now)
